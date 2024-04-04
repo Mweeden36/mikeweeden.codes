2095c589996d37641449a285fedf206929508007
Mike Weeden <weeden.mike@gmail.com>
2024-04-04 08:28:08 -0400

    Adding a page with a link since redirects are not supported for exported next apps

---
+++ src/app/ffl/page.tsx
@@ -0,0 +1,16 @@
+export default function FFL() {
+  return (
+    <div className="flex-auto self-center p-6">
+      Please fill out the
+      <a
+        className="text-silver dark:text-primary"
+        href="https://docs.google.com/forms/d/e/1FAIpQLScAy9aNdAsaKqFaE5YwaoGteJ-FqCQoY88_BCR8sw6oF2TAeA/viewform"
+        target="_blank"
+      >
+        {" "}
+        Rule change Form{" "}
+      </a>
+      to propose a new rule change for the league.
+    </div>
+  );
+}
